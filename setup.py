ad6152d1d80bd43106cf39c8ae480263c7926192
Camilo Nova <camilo.nova@axiacore.com>
2014-03-13 15:09:09 -0500

    Added setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='numbertoletter',
+    version='0.1',
+    description='A python library to convert a number to letters',
+    url='https://github.com/AxiaCore/number-to-letters',
+    author='AxiaCore',
+    author_email='info@axiacore.com',
+    license='MIT',
+    packages=['numbertoletter'],
+    zip_safe=False,
+)
